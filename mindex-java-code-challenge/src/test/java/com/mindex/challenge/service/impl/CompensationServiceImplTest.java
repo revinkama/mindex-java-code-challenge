75cffd071bd6241b305a20457c5e24c31e0647b5
Kevin Rama <37127120+revinkama@users.noreply.github.com>
2023-03-16 16:09:49 -0500

    Add files via upload

---
+++ mindex-java-code-challenge/src/test/java/com/mindex/challenge/service/impl/CompensationServiceImplTest.java
@@ -0,0 +1,65 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.CompensationService;
+import com.mindex.challenge.service.ReportStructureService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CompensationServiceImplTest {
+
+    private String compensationUrl;
+    @Autowired
+    private CompensationService compensationService;
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setup() {
+        compensationUrl = "http://localhost:" + port + "/compensation";
+    }
+
+    @Test
+    public void testCreateCompensation() {
+
+        Employee testEmployee1 = new Employee();
+        testEmployee1.setEmployeeId("123455");
+        testEmployee1.setFirstName("John");
+        testEmployee1.setLastName("Doe");
+        testEmployee1.setDepartment("Engineering");
+        testEmployee1.setPosition("Developer");
+
+        Compensation compensation = new Compensation();
+        compensation.setEmployee(testEmployee1);
+        compensation.setSalary(123000);
+        compensation.setEffectiveDate(Instant.parse("2020-02-22T16:37:23Z"));
+
+        Compensation compensationCall = compensationService.create(compensation);
+        assertNotNull(compensationCall.getEmployee());
+        assertNotNull(compensationCall.getSalary());
+        assertNotNull(compensationCall.getEffectiveDate());
+
+    }
+}
